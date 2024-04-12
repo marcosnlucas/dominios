ae9181c51ea16722fcadbd7c9db899fee5c444da
marcosnlucas <98777375+marcosnlucas@users.noreply.github.com>
2024-04-12 18:41:01 -0300

    Update dominios.py

--- dominios.py
+++ dominios.py
@@ -9,14 +9,17 @@ def check_domain(domain):
         # Usamos st.write para depuração visível na interface do usuário
         st.write(f"WHOIS para {domain}: {domain_info}")
 
+        # Verifica se a resposta WHOIS indica que o domínio não está registrado
         if domain_info.status is None and domain_info.expiration_date is None:
             return True
         if isinstance(domain_info.status, list):
-            if any("available" in s.lower() for s in domain_info.status):
+            if any("no match" in s.lower() or "available" in s.lower() for s in domain_info.status):
                 return True
         elif domain_info.status:
             if "no match" in domain_info.status.lower() or "available" in domain_info.status.lower():
                 return True
+        if "no match for" in str(domain_info).lower():
+            return True
         return False
     except Exception as e:
         st.write(f"Erro ao verificar o domínio {domain}: {e}")
